14501bfb3b26433a10343a85bc1729db36558b09
brunomeloesilva <brunomeloesilva@gmail.com>
2021-04-21 20:52:13 -0400

    Configurando token JWT no Swagger para fazer requisições com autenticação

--- src/main/java/github/com/brunomeloesilva/config/swagger/SwaggerConfigurations.java
+++ src/main/java/github/com/brunomeloesilva/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,15 @@
 package github.com.brunomeloesilva.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import github.com.brunomeloesilva.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +23,15 @@ public class SwaggerConfigurations {
 				.apis(RequestHandlerSelectors.basePackage("github.com.brunomeloesilva"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(Arrays.asList(new ParameterBuilder()
+															.name("Authorization")
+															.description("Header para token JWT")
+															.modelRef(new ModelRef("string"))
+															.parameterType("header")
+															.required(false)
+															.build()));
 	}
 }
+
+//globalOperationParameters = é um parametro que eu quero que o Swagger apresente em todos os EndPoints.
